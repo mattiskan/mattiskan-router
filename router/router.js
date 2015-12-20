8e32845ee17c060d622508dc07a4b70f7232ddc7
Mattis Kancans Envall <dev@mattiskan.se>
2015-12-20 21:17:57 +0100

    added error emails to router

--- router/router.js
+++ router/router.js
@@ -1,24 +1,27 @@
+var mail = require('mailgun-send');
 var proxy = require('http-proxy').createProxy();
 var fs = require('fs');
 
+var config = JSON.parse(
+  fs.readFileSync('router/config.json', 'utf-8').replace(/\$HOST/g, process.env.HOST_IP || '172.0.0.1')
+);
+mail.config(config.mail);
 
 function translate(requestedHostname){
-  var config = JSON.parse(
-    fs.readFileSync('router/config.json', 'utf-8').replace(/\$HOST/g, process.env.HOST_IP || '172.0.0.1')
-  );
-
-  for( regex in config.routes ) {
+  for(regex in config.routes) {
     if(new RegExp(regex).test(requestedHostname)){
       return config.routes[regex];
     }
   }
 
-  return config.default;
+  throw new Error("Unmatched domain: " + requestedHostname);
 };
 
 var router = require('http').createServer(function(req, res) {
   console.log('['+req.method+'] ' + req.headers.host + req.url);
 
+  
+  
   proxy.web(req, res, {
     target: translate(req.headers.host)
   });
@@ -38,8 +41,14 @@ proxy.on('error', function (err, req, res) {
 });
 
 process.on('uncaughtException', function(err) {
-    //för att det blir lite dålig stäming om redirectservern ligger nere
-    console.log("CRITICAL:" + err);
+  //för att det blir lite dålig stäming om redirectservern ligger nere
+  console.log("CRITICAL:", err);
+
+  mail.send({
+    subject: 'Error in router',
+    recipient: 'router+error@mattiskan.se',
+    body: '' + err
+  });
 });
 
 router.listen(8080);
