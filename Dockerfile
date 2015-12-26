5854e1576d42466a2f6dcc1e52b2aeca3a314eb4
Mattis Kancans Envall <dev@mattiskan.se>
2015-12-26 17:20:50 +0100

    gonna use docker logs instead of own logging

--- Dockerfile
+++ Dockerfile
@@ -5,4 +5,4 @@ COPY package.json /src/package.json
 RUN npm install
 COPY ./router/ /src/router
 
-CMD node router/router.js 2>&1 | tee /log.file
+CMD node router/router.js
--- restart.sh
+++ restart.sh
@@ -2,7 +2,6 @@
 docker build -t redirect-server .
 docker kill $(cat running.container) || true
 docker run -d -p 80:8080 --restart=always \
-       -v /var/log/mattiskan/router.log:/log.file \
        -e HOST_IP=$(ip route get 1 | awk '{print $NF;exit}') \
        redirect-server > running.container
 docker logs -f $(cat running.container)
