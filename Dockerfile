16430d7f6a4b0afd02d3d076f79a7ba951642618
Mattis Kancans Envall <hello@mattiskan.se>
2017-04-01 21:23:23 +0000

    router now supports https

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,16 @@
-FROM node
+FROM  ubuntu:latest
+
+ENTRYPOINT ["/usr/bin/dumb-init", "--"]
+
+RUN apt-get update && apt-get install -y nodejs npm wget
+RUN wget https://github.com/Yelp/dumb-init/releases/download/v1.2.0/dumb-init_1.2.0_amd64.deb
+RUN dpkg -i dumb-init_*.deb
+
 EXPOSE  8080
+
 WORKDIR /src
 COPY package.json /src/package.json
 RUN npm install
 COPY ./router/ /src/router
 
-CMD ["node", "router/router.js"]
+CMD ["nodejs", "router/router.js"]
--- router/router.js
+++ router/router.js
@@ -1,11 +1,14 @@
-var mail = require('mailgun-send');
+//var mail = require('mailgun-send');
 var proxy = require('http-proxy').createProxy();
 var fs = require('fs');
+var tls = require('tls');
+var http = require('http');
+var https = require('https');
 
 var config = JSON.parse(
   fs.readFileSync('router/config.json', 'utf-8').replace(/\$HOST/g, process.env.HOST_IP || '172.0.0.1')
 );
-mail.config(config.mail);
+//mail.config(config.mail);
 
 function translate(requestedHostname){
   requestedHostname = (requestedHostname || '').toLowerCase();
@@ -19,7 +22,7 @@ function translate(requestedHostname){
   return 404;
 };
 
-var router = require('http').createServer(function(req, res) {
+var requestHandler = function(req, res) {
   var ip = req.headers['x-forwarded-for'] ||
       req.connection.remoteAddress ||
       req.socket.remoteAddress ||
@@ -39,7 +42,7 @@ var router = require('http').createServer(function(req, res) {
     xfwd: true, // pass clients ip as req.headers['x-forwarded-for']
     //ws: true, // forward websockets
   });
-});
+};
 
 var errorFunction = function (err, req, res) {
   console.log(err);
@@ -48,7 +51,7 @@ var errorFunction = function (err, req, res) {
   res.writeHead(500, {
     'Content-Type': 'text/html'
   });
-  res.end("<img src='http://i.imgur.com/VD0EDRD.gif'>"+
+  res.end("<img src='http://gifrific.com/wp-content/uploads/2012/07/blender-explode-home-improvement.gif'>"+
           "<h2>Oops</h2>Looks like there's some problems... "+
           "I have already been automatically notified, so there's nothing to do but to wait.");
 
@@ -57,11 +60,12 @@ var errorFunction = function (err, req, res) {
   if(err.code === 'ECONNRESET')
     return;
   
+  /*console.log('sent email to ' + config.notificationEmail);
   mail.send({
     subject: 'Failing service: ' + req.headers.host,
     recipient: config.notificationEmail,
     body: '' + err
-  });
+  }); */
 };
 
 var write404 = function(res) {
@@ -70,18 +74,51 @@ var write404 = function(res) {
           "<h2>404 — I can't find the page you're requesting.</h2>");
 };
 
+var secureContext = {
+    'swagyolo.biz': {
+	key: fs.readFileSync('router/certs/swagyolo.biz.key'),
+	cert: fs.readFileSync('router/certs/swagyolo.biz.crt'),
+    },
+    'mattiskan.se': {
+	key: fs.readFileSync('router/certs/mattiskan.se.key'),
+	cert: fs.readFileSync('router/certs/mattiskan.se.crt'),
+    },
+};
+
+var options = {
+    key: fs.readFileSync('router/certs/swagyolo.biz.key'),
+    cert: fs.readFileSync('router/certs/swagyolo.biz.crt'),
+    SNICallback: function(domain, cb) {
+        if (secureContext[domain]) {
+            if (cb) {
+                return cb(null, tls.createSecureContext(secureContext[domain]));
+            } else {
+                // compatibility for older versions of node
+                return secureContext[domain]; 
+            }
+        } else {
+            throw new Error('No keys/certificates for domain requested');
+        }
+    },
+};
+
+
+var router = http.createServer(requestHandler);
+var secureRouter = https.createServer(options, requestHandler);
+
 proxy.on('error', errorFunction);
 router.on('error', errorFunction);
+secureRouter.on('error', errorFunction);
 
 process.on('uncaughtException', function(err) {
   //för att det blir lite dålig stäming om redirectservern ligger nere
-  console.log("CRITICAL:", err);
+  console.log("unrecoverable:", err);
   
-  mail.send({
-    subject: 'CRITICAL: UncaughtException in router',
+  /* mail.send({
+    subject: 'UncaughtException in router',
     recipient: config.notificationAddress,
     body: '' + err
-  });
+  }); */
 });
 
 process.on('SIGTERM', function(){
@@ -92,7 +129,9 @@ process.on('SIGTERM', function(){
   });
 });
 
-
 router.listen(8080, function() {
   console.log("router started. Using host ip:", process.env.HOST_IP || '172.0.0.1')
+});
+secureRouter.listen(8081, function() {
+  console.log("secure router started. Using host ip:", process.env.HOST_IP || '172.0.0.1')
 });
